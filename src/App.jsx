c7183d27e40d89d61ca68b43277721e198ad6ea6
Richard Bujdoso <ricsi.bujdoso@gmail.com>
2024-07-02 23:01:14 +0200

    progression_2
    https://youtu.be/HFj5FMb0jwY?si=MMmjRukiolnvILTH&t=2688

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import HomePage from './routes/homePage/homePage'
 import Navbar from './components/navbar/Navbar'
 import './layout.scss'
 
 function App() {
   return (
     <div className="layout">
+      <div className="navbar">
       <Navbar />
+      </div>
+      <div className="content">
+        <HomePage/>
+      </div>
     </div>
   )
 }

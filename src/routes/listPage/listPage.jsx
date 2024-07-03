889d32c06feadcda44d74222775826a52872d6be
Richard Bujdoso <ricsi.bujdoso@gmail.com>
2024-07-03 22:22:32 +0200

    progression_5
    https://youtu.be/HFj5FMb0jwY?si=UWkp-Vf_Kqg7nqg8&t=7965

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,7 +1,26 @@
+import Card from "../../components/card/Card";
+import Filter from "../../components/filter/Filter";
+import Map from "../../components/map/Map";
+import { listData } from "../../lib/dummydata";
 import "./listPage.scss";
 
 function ListPage() {
-  return <div className="listPage">ListPage</div>;
+  const DATA = listData;
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {DATA.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+        <Map items={DATA} />
+      </div>
+    </div>
+  );
 }
 
 export default ListPage;
